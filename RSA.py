dbaa8465a713111ee97c4e6091339af3d40c106b
pavi754 <127126060+pavi754@users.noreply.github.com>
2024-10-17 13:59:08 +0530

    Create RSA.py

---
+++ RSA.py
@@ -0,0 +1,55 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def modinv(e, phi):
+    d, x1, x2, y1 = 0, 0, 1, 1
+    temp_phi = phi
+    while e > 0:
+        temp1, temp2 = divmod(temp_phi, e)
+        temp_phi, e = e, temp2
+        x = x2 - temp1 * x1
+        y = d - temp1 * y1
+        x2, x1 = x1, x
+        d, y1 = y1, y
+    if temp_phi == 1:
+        return d + phi
+
+def generate_key(p, q):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 17
+    g = gcd(e, phi)
+   
+    while g != 1:
+        e += 1
+        g = gcd(e, phi)
+
+    d = modinv(e, phi)
+    return (e, n), (d, n)
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    return cipher
+
+def decrypt(pk, ciphertext):
+    key, n = pk
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+    return ''.join(plain)
+
+if __name__ == "__main__":
+    p = int(input("Enter the prime number (p): "))
+    q = int(input("Enter another prime number (q): "))
+   
+    public, private = generate_key(p, q)
+    print(f"Public key: {public}")
+    print(f"Private key: {private}")
+   
+    message = input("Enter the message to encrypt: ")
+    encrypt_msg = encrypt(public, message)
+    print(f"Encrypted message: {encrypt_msg}")
+   
+    decrypted_msg = decrypt(private, encrypt_msg)
+    print(f"Decrypted message: {decrypted_msg}")
